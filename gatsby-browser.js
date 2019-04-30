ed831f76f423ab965e482e8cd5546b8e42b54192
Luiz Damim <luizvd@gmail.com>
2019-04-30 17:31:39 -0300

    Syntax highlighting (needs improvement. a lot.)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,8 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+
+require("prismjs/themes/prism.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+// require("prismjs/themes/prism-tomorrow.css")
+// require("prismjs/themes/prism-solarizedlight.css")
